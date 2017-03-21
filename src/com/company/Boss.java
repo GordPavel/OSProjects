d13be4df9f80c460496e314b35b65f760e2a35c7
posohaka <samara.posohaka@mail.ru>
2017-03-21 10:56:37 +0400

    Update Boss.java

--- src/com/company/Boss.java
+++ src/com/company/Boss.java
@@ -1,8 +1,24 @@
 package com.company;
 
+import java.util.Scanner;
+
 /**
  * Created by s3rius on 21.03.17.
  */
 public class Boss extends Thread {
 
+    private String Bossstr;
+
+    @Override
+    public  void run()
+    {
+        System.out.println("Введите колличество процоссов");
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        for (int i = 0; i < n; i++) {
+            Scout sc = new Scout(in);
+            
+        }
+    }
 }
+
