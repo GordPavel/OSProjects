d310747d56009b3b6a7dd744968b40d1bd077442
s3rius <win10@list.ru>
2017-03-21 10:52:47 +0400

    Scout class written

--- src/com/company/Scout.java
+++ src/com/company/Scout.java
@@ -1,7 +1,25 @@
 package com.company;
 
+import java.util.Scanner;
+
 /**
  * Created by s3rius on 21.03.17.
  */
 public class Scout extends Thread {
+
+    private String string;
+
+    public Scout(Scanner scanner){
+        string = scanner.nextLine();
+    }
+
+//    public String getString() {
+//        return string;
+//    }
+
+
+    public void run(String sc) {
+        super.run();
+        sc += string;
+    }
 }
