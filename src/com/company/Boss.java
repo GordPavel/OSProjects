a919d5aa97fece6057326663459ff311d82e8679
Павел Васильевич Гордеев <pvgord@icloud.com>
2017-03-27 12:57:59 +0400

    Update Boss.java
    I edit your fucking shit go and try it yourself, scum.

--- src/com/company/Boss.java
+++ src/com/company/Boss.java
@@ -3,37 +3,54 @@ package com.company;
 import java.util.Scanner;
 import java.util.concurrent.Semaphore;
 
-/**
- * Created by s3rius on 21.03.17.
- */
-public class Boss extends Thread {
+public class Boss extends Thread{
 
-    private String bossstr;
-    private Semaphore semaphore;
+    String bossString = "";
 
     @Override
-    public  void run()
-    {
-        semaphore = new Semaphore(2, true);
-        bossstr = "";
-        Scanner in = new Scanner(System.in);
-        try {
-            semaphore.acquire();
-
-        System.out.println("Введите колличество процоссов");
-
-        int n = in.nextInt();
-            for (int i = 0; i < n ; i++) {
-                Scout sc = new Scout(in);
-                sc.run();
-                bossstr += sc.getString();
-            }
-            System.out.println(bossstr);
-
-        semaphore.release();
-        } catch (InterruptedException e) {
-        e.printStackTrace();
-    }
+    public void run(){
+        Scanner   bossScanner   = new Scanner( System.in );
+        Scanner   scoutsScanner = new Scanner( System.in );
+        Semaphore semaphore     = new Semaphore( 2 );
+        System.out.println( "Введите кол-во Шпиёнаф, бистра! Швуйне, швуйне!" );
+        for( int i = 0, scoutsQ = bossScanner.nextInt() ; i < scoutsQ ; i++ ){
+            Thread scout = new Thread( new Scout( i + 1, this, semaphore, scoutsScanner ) );
+            scout.run();
+        }
+        bossScanner.close();
+        System.out.println( bossString );
     }
 }
+
+class Scout implements Runnable{
+
+    Integer   number;
+    Boss boss;
+    Semaphore semaphore;
+    Scanner   scanner;
+
+    public Scout( Integer number, Boss boss, Semaphore semaphore, Scanner scanner ){
+        this.number = number;
+        this.boss = boss;
+        this.semaphore = semaphore;
+        this.scanner = scanner;
+    }
+
+    @Override
+    public void run(){
+        try{
+            semaphore.acquire();
+
+            System.out.println( "Я шпиён " + number + ". Дай мне число." );
+            boss.bossString += scanner.nextLine() + " ";
+
+            semaphore.release();
+        }catch( InterruptedException e ){
+            e.printStackTrace();
+        }
+    }
+}
+
+
+
 
